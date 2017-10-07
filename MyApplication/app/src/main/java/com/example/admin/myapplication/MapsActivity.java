1003e99f827ab27f8ea84cd3db1d9d0dfed0bc0e
MaScFx <bior@ukr.net>
2017-10-07 14:29:47 +0300

    add l2

--- MyApplication/app/src/main/java/com/example/admin/myapplication/MapsActivity.java
+++ MyApplication/app/src/main/java/com/example/admin/myapplication/MapsActivity.java
@@ -37,12 +37,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-        mMap.setOnCameraIdleListener(new GoogleMap.OnCameraIdleListener() {
-            @Override
-            public void onCameraIdle() {
-
-            }
-        });
+//        mMap.setOnCameraIdleListener(new GoogleMap.OnCameraIdleListener() {
+//            @Override
+//            public void onCameraIdle() {
+//
+//            }
+//        });
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
